0f9940fed4a13189b84e022483137e903d09e12f
Denilson <denilsoncastro@outlook.com>
2019-07-09 11:11:28 -0300

    Init do Projeto

---
+++ segundo-projeto/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>hkhklhkhkhk</Text>
+
+      <Button
+        onPress={() => {
+          alert('You tapped the button!');
+        }}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
